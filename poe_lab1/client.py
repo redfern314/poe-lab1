8a6e06b4c17f76dcd2fc6d23243ec90088f82c2a
Derek Redfern <redfern.derek@gmail.com>
2013-02-10 15:24:10 -0500

    Serial handshake

--- poe_lab1/client.py
+++ poe_lab1/client.py
@@ -3,21 +3,36 @@
 
 import serial #serial library
 from serial.tools import list_ports
+import sys
 
-def connectToArduino(available):
+def connectToArduino():
     pass #dummy code
     #loop through avail serial ports
-    flag=True
+    connected=False
 
-    while flag:
+    for i in range(100):
+        available=findAvailPorts()
+        msg=""
         for avail in available:
             s=serial.Serial(avail, 9600, timeout=1)
             w=s.inWaiting()
-            #print w
-            r=s.read(10)
-            print r
-            if(r==5):
-                print avail+': '+r
+            
+            r=""
+            while w>0 and r!='\n':
+                msg+=r
+                r=s.read(1)
+                w=s.inWaiting()   
+
+            print msg
+            if(msg.strip()=='Arduino Ready' or msg.strip()=='Connected\n'):
+                #connected=True
+                print avail+': '+msg.strip()+'!!!!'
+                s.write('Python Ready\n')
+
+            if r=='\n':
+                msg=""  
+
+    return None
 
 def findAvailPorts():
     available=[]
@@ -32,5 +47,3 @@ def findAvailPorts():
             pass
 
     return available
-
-connectToArduino(findAvailPorts())
