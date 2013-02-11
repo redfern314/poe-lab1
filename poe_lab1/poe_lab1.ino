5813a2029ed73697a5b5e7c55f51d995d3840e36
Halie <hdavis@CTVPFV1.olin.edu>
2013-02-10 22:18:45 -0500

    Servo motion working w/ commenting.

--- poe_lab1/poe_lab1.ino
+++ poe_lab1/poe_lab1.ino
@@ -29,8 +29,8 @@ double vectorsend;
 
 //stuff from serial port:
 //char start;
-int precision;
-int angle;
+int precision=10;
+int angle=30;
 
 
 void setup() {
@@ -44,27 +44,38 @@ void setup() {
   counter=0;       //defining variables. *counter not necessary, yet, but I think we'll need it.
 }
 
-void movement(){
-  for(pos=0; pos<180; pos = pos + precision) {
+void loop() { //movement(){
+  for(pos==0; pos<180; pos = pos + round(precision/9)) {
     servo1.write(pos);
-    delay (50*precision^-1);
-        if ((pos==0 || pos==180) && (pos2==(180-(angle/2)) || pos2==(90-(angle/2)))) {
-          pos2=pos2+precision;
-          servo2.write(pos2);
-          delay (15);
+    delay (50/precision);
+        if ((pos==0 || pos==180)){
+         // if ((pos2==(180-(angle/2)) || pos2==(90-(angle/2)))) {
+            delay (70);
+            pos2=pos2+precision;
+            servo2.write(pos2);
+            delay (30);
+         //}
         }
     }
     
-  for (pos=180; pos>0; pos=pos - precision) {
+  for (pos==180; pos>0; pos = pos - round(precision/9)) {
     servo1.write(pos);
-    delay (50*precision^-1);
-        if ( (pos==0 || pos==180) && (pos2==(180-(angle/2)) || pos2==(90-(angle/2))) ) {
-          pos2=pos2+precision;
-          servo2.write(pos2);
-          delay (15);
-        }
-  }
+    delay (50/precision);
+       // if ( (pos==0 || pos==180) ){
+          delay (30);
+           if (pos2==(180-(angle/2)) || pos2==(90-(angle/2)))  {
+              pos2=pos2+precision;
+              servo2.write(pos2);
+              delay (30);
+        }} //}
   
+  /*if (pos2==angle){
+    pos=0;
+    pos2=0;
+  }
+  */
+}
+ /* 
   if (pos2>=0 && pos2<=180) {
     
     
@@ -73,15 +84,14 @@ void movement(){
       vectorsend[0]=light;
     }
 
-  }
-}
+  } */
 
-void loop(){
-  portOne.listen();
-  Serial.println("Data from port one:");
-  while (portOne.available()>0) {
+//void loop(){
+ // portOne.listen();
+  //Serial.println("Data from port one:");
+  //while (portOne.available()>0) {
     
-    if (Serial.read()>=0) {
+    /*if (Serial.read()>=0) {
       
       char c=Serial.read();
       char d=Serial.read();
@@ -94,9 +104,10 @@ void loop(){
       int A= (int) (a); 
       int B = (int) (b);
       angle='A' + 'B';
-      Serial.print(angle);
+      Serial.print(angle);}*/
        
-      movement();  //calls previously defined movement function.
+      //movement();  //calls previously defined movement function.
+ // }
       
-    }
-    else {}}}
+    //}
+    //else {}}}
