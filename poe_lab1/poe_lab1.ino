946e4cf766e9b9fd9a7fbb121b80f5832f395b6e
Halie <hdavis@CTVPFV1.olin.edu>
2013-02-11 00:35:41 -0500

    Comments!

--- poe_lab1/poe_lab1.ino
+++ poe_lab1/poe_lab1.ino
@@ -20,9 +20,7 @@ int pos2;
 
 String data;
 double photoresistor=1; //analog in chanel for photo resistor
-int stepSize;
 int i=0; //counter for data acquision from photo resistor.
-double vectorsend;
 
 //stuff from serial port:
 char start;
@@ -31,17 +29,17 @@ int angle;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);     //start serial port so we can write data to it.
   servo1.attach(3);       //digital PWM pins for signal to the two servos
   servo2.attach(5);
-  pinMode(photoresistor, INPUT);
+  pinMode(photoresistor, INPUT);    //set photoresisstor up as an input
 }
 
-void movement(){
-  for(pos==0; pos<180; pos = pos + round(precision/9)) {
+void movement(){        //define a function which will move the servos in a specified pattern.
+  for(pos==0; pos<180; pos = pos + round(precision/9)) {    //Step through an arch for the bottom servo.
     servo1.write(pos);
     delay (50/precision);
-        if ((pos==0 || pos==180)){
+        if ((pos==0 || pos==180)){    //move the photo resistor up a step (size of which is determined by the percision value) at ends of arc.
             delay (70);
             pos2=pos2+precision;
             servo2.write(pos2);
@@ -49,25 +47,24 @@ void movement(){
         }
     }
     
-  for (pos==180; pos>0; pos = pos - round(precision/9)) {
+  for (pos==180; pos>0; pos = pos - round(precision/9)) {     //this case accomplishes movement in the opposite direction
     servo1.write(pos);
     delay (50/precision);
-          delay (30);
-           if (pos2==(180-(angle/2)) || pos2==(90-(angle/2)))  {
-              pos2=pos2+precision;
-              servo2.write(pos2);
-              delay (30);
-           }
+      if (pos2==(180-(angle/2)) || pos2==(90-(angle/2)))  {
+      pos2=pos2+precision;
+      servo2.write(pos2);
+      delay (30);
+      }
   }
 }
 
-void returndata(){
-    int light=analogRead(photoresistor);
-    String e = String (pos); 
+void returndata(){      //sets up a function which writes data to the serial port so we can use it in the python code to make a graph.
+    int light=analogRead(photoresistor);    //reads the light from the photoresistor.
+    String e = String (pos);      //change the position integers into strings:
     String f = String (pos2); 
     String g = String (light);
-    String data = e+f+g;
-    Serial.println(data);
+    String data = e+f+g;      //combines the strings into a single sting
+    Serial.println(data);     //prints data to the serial port
 }
 
 void loop(){
